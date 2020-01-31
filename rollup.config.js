ff98ac06673437fb3c7d8f49930e0701e150a2f4
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-01-31 11:17:22 -0500

    Add esm build. Adjust to cherry picked gl-matrix modules.

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,23 @@
 import { terser } from "rollup-plugin-terser";
-import buble from "rollup-plugin-buble";
+import buble from "@rollup/plugin-buble";
 
-const config = (file, format, plugins) => ({
+const config = (file, format, plugins = []) => ({
   input: "src/index.js",
   output: {
     name: "createCamera2d",
     format,
     file,
     globals: {
-      "gl-matrix": "glMatrix"
+      "gl-matrix/mat4": "glMatrix.mat4",
+      "gl-matrix/vec4": "glMatrix.vec4"
     }
   },
   plugins,
-  external: ["gl-matrix"]
+  external: ["gl-matrix/mat4", "gl-matrix/vec4"]
 });
 
 export default [
   config("dist/camera-2d.js", "umd", [buble()]),
-  config("dist/camera-2d.min.js", "umd", [buble(), terser()])
+  config("dist/camera-2d.min.js", "umd", [buble(), terser()]),
+  config("dist/camera-2d.esm.js", "esm")
 ];
